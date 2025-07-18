9e98392fffd1dab99d5c9e4c1668af1cfabe310d
Javier Sierra <javier.sierra@oc.vasscompany.com>
2025-07-18 20:08:55 +0200

    chore: remove comment form

--- comments.php
+++ comments.php
@@ -19,8 +19,7 @@ if ( post_password_required() ) {
 	return;
 }
 ?>
-
-<div id="comments" class="comments-area">
+<div id="comments" class="comments-area" style="display: none;>
 
 	<?php
 	// You can start editing here -- including this comment!
